287bf8edf558743fd2e256db8640288d3d2d1e46
samhanx <xuanxinhan@vip.qq.com>
2018-02-12 16:14:15 +0800

    [feat] 头尾样式和几本结构

--- basic/src/scripts/index.js
+++ basic/src/scripts/index.js
@@ -2,8 +2,33 @@ import util from './util'
 
 const l = console.log.bind(console)
 
-l(util.createMatrix(0))
+// l(util.createMatrix(0))
 
-let arr = Array.from({length: 9}, (v, i) => i + 1)
-l(arr)
-l(util.shuffle(arr))
+// let arr = Array.from({length: 9}, (v, i) => i + 1)
+// l(arr)
+// l(util.shuffle(arr))
+
+class Grid {
+  constructor(container) {
+    this.$container = container
+  }
+
+  build() {
+    const matrix = util.createMatrix()
+    
+    const $cells = matrix.map(rowArrays => rowArrays.map(cellVal => {
+      return $(`<span>${cellVal}</span>`)
+      // return $('<span>').text(cellVal)
+    }))
+
+    const $divArr = $cells.map($spanArr => {
+      return $('<div>').append($spanArr)
+    })
+
+    this.$container.append($divArr)
+  }
+
+
+}
+
+new Grid($('#container')).build()
